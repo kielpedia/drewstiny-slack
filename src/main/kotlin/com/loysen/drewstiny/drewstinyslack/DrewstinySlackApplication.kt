63d2467953504cadeeb685c05212510372565e38
Kiel Loysen <kloysen@grubhub.com>
2017-10-02 09:22:45 -0500

    fix startup context error

--- src/main/kotlin/com/loysen/drewstiny/drewstinyslack/DrewstinySlackApplication.kt
+++ src/main/kotlin/com/loysen/drewstiny/drewstinyslack/DrewstinySlackApplication.kt
@@ -2,7 +2,9 @@ package com.loysen.drewstiny.drewstinyslack
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ComponentScan
 
+@ComponentScan("com.loysen.drewstiny.drewstinyslack.config")
 @SpringBootApplication
 class DrewstinySlackApplication
 
